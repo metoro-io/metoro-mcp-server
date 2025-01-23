aa3511f4038b1f6fbece730e94ec884028b47082
Chris Battarbee <chris@metoro.io>
2025-01-23 12:39:45 +0000

    make namespaces more useful

--- tools/get_namespaces.go
+++ tools/get_namespaces.go
@@ -3,21 +3,29 @@ package tools
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	mcpgolang "github.com/metoro-io/mcp-golang"
 	"github.com/metoro-io/metoro-mcp-server/utils"
 )
 
-type GetNamespacesHandlerArgs struct{}
+type GetNamespacesHandlerArgs struct {
+	Environments []string `json:"environments" jsonschema:"description=The environments (aka clusters) to get namespaces for. If empty all environments will be used."`
+}
 
 func GetNamespacesHandler(ctx context.Context, arguments GetNamespacesHandlerArgs) (*mcpgolang.ToolResponse, error) {
-	body, err := getNamespacesMetoroCall(ctx)
+	body, err := getNamespacesMetoroCall(ctx, arguments)
 	if err != nil {
 		return nil, fmt.Errorf("error getting namespaces: %v", err)
 	}
 	return mcpgolang.NewToolResponse(mcpgolang.NewTextContent(fmt.Sprintf("%s", string(body)))), nil
 }
 
-func getNamespacesMetoroCall(ctx context.Context) ([]byte, error) {
-	return utils.MakeMetoroAPIRequest("GET", "namespaces", nil, utils.GetAPIRequirementsFromRequest(ctx))
+func getNamespacesMetoroCall(ctx context.Context, arguments GetNamespacesHandlerArgs) ([]byte, error) {
+	endpoint := "namespaces"
+	if len(arguments.Environments) > 0 {
+		endpoint = fmt.Sprintf("namespaces?environments=%s", strings.Join(arguments.Environments, ","))
+	}
+
+	return utils.MakeMetoroAPIRequest("GET", endpoint, nil, utils.GetAPIRequirementsFromRequest(ctx))
 }
