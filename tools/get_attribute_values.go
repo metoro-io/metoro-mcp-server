3b40c159f51fb1c891072cd2e13fb9c5df3749cf
Chris Battarbee <chris@metoro.io>
2025-04-01 22:56:31 +0100

    Fix up some of the fields

--- tools/get_attribute_values.go
+++ tools/get_attribute_values.go
@@ -11,12 +11,11 @@ import (
 )
 
 type GetAttributeValuesHandlerArgs struct {
-	Type           model.MetricType    `json:"type" jsonschema:"required,description=The type of telemetry data to get the attribute keys and values for. Either 'logs' or 'trace' or 'metric' or 'kubernetes_resource'"`
-	TimeConfig     utils.TimeConfig    `json:"time_config" jsonschema:"required,description=The time period to use while getting the possible values of log attributes. e.g. if you want to get values for the last 5 minutes you would set time_period=5 and time_window=Minutes. You can also set an absoulute time range by setting start_time and end_time"`
-	Attribute      string              `json:"attribute" jsonschema:"required,description=The attribute key to get the possible values for. Possible values for attribute should be obtained from get_attribute_keys tool call for the same type"`
-	MetricName     string              `json:"metricName" jsonschema:"description=REQUIRED IF THE TYPE IS 'metric'. The name of the metric to get the possible attribute keys and values."`
-	Filters        map[string][]string `json:"filters" jsonschema:"description=The filters to apply before getting the possible values. For example if you want to get the possible values for attribute key service.name where the environment is X you would set the Filters as {environment: [X]}"`
-	ExcludeFilters map[string][]string `json:"excludeFilters" jsonschema:"description=The exclude filters to exclude/eliminate possible values an attribute can take. Log attributes matching the exclude filters will not be returned. For example if you want the possible values for attribute key service.name where the attribute environment is not X then you would set the ExcludeFilters as {environment: [X]}"`
+	Type       model.MetricType    `json:"type" jsonschema:"required,description=The type of telemetry data to get the attribute keys and values for. Either 'logs' or 'trace' or 'metric' or 'kubernetes_resource'"`
+	TimeConfig utils.TimeConfig    `json:"time_config" jsonschema:"required,description=The time period to use while getting the possible values of log attributes. e.g. if you want to get values for the last 5 minutes you would set time_period=5 and time_window=Minutes. You can also set an absoulute time range by setting start_time and end_time"`
+	Attribute  string              `json:"attribute" jsonschema:"required,description=The attribute key to get the possible values for. Possible values for attribute should be obtained from get_attribute_keys tool call for the same type"`
+	MetricName string              `json:"metricName" jsonschema:"description=REQUIRED IF THE TYPE IS 'metric'. The name of the metric to get the possible attribute keys and values."`
+	Filters    map[string][]string `json:"filters" jsonschema:"description=The filters to apply before getting the possible values. For example if you want to get the possible values for an attribute key where the environment is X you would set the Filters as {environment: [X]}"`
 }
 
 func GetAttributeValuesHandler(ctx context.Context, arguments GetAttributeValuesHandlerArgs) (*mcpgolang.ToolResponse, error) {
@@ -32,19 +31,17 @@ func GetAttributeValuesHandler(ctx context.Context, arguments GetAttributeValues
 	switch arguments.Type {
 	case model.Logs:
 		modelRequest := model.LogSummaryRequest{
-			StartTime:      startTime,
-			EndTime:        endTime,
-			Filters:        arguments.Filters,
-			ExcludeFilters: arguments.ExcludeFilters,
+			StartTime: startTime,
+			EndTime:   endTime,
+			Filters:   arguments.Filters,
 		}
 		request.Logs = &modelRequest
 		break
 	case model.Trace:
 		modelRequest := model.TracesSummaryRequest{
-			StartTime:      startTime,
-			EndTime:        endTime,
-			Filters:        arguments.Filters,
-			ExcludeFilters: arguments.ExcludeFilters,
+			StartTime: startTime,
+			EndTime:   endTime,
+			Filters:   arguments.Filters,
 		}
 		request.Trace = &modelRequest
 		break
