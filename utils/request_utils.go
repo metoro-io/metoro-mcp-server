2b0e7baf710d46edcccdffd9ce7ef0487b48ea55
Chris Battarbee <chris@metoro.io>
2025-07-01 21:29:54 +0100

    Fix error handling

--- utils/request_utils.go
+++ utils/request_utils.go
@@ -73,7 +73,7 @@ func MakeMetoroAPIRequest(method, endpoint string, body io.Reader, apiRequiremen
 	}
 
 	// Check the response status code
-	if resp.StatusCode != http.StatusOK {
+	if resp.StatusCode >= 300 {
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
