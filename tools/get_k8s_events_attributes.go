f2139645bbfe1ae326740da373c1508d928151f5
Chris Battarbee <chris@chrisbattarbee.com>
2026-02-26 11:23:39 +0000

    Add required time_config to core k8s metadata tools

--- tools/get_k8s_events_attributes.go
+++ tools/get_k8s_events_attributes.go
@@ -3,18 +3,40 @@ package tools
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strconv"
 
 	mcpgolang "github.com/metoro-io/mcp-golang"
 	"github.com/metoro-io/metoro-mcp-server/utils"
 )
 
-type GetK8sEventsAttributesHandlerArgs struct{}
+type GetK8sEventsAttributesHandlerArgs struct {
+	TimeConfig utils.TimeConfig `json:"time_config" jsonschema:"required,description=The time period to get Kubernetes event attributes for. You can set relative or absolute time range using time_config"`
+}
 
 func GetK8sEventsAttributesHandler(ctx context.Context, arguments GetK8sEventsAttributesHandlerArgs) (*mcpgolang.ToolResponse, error) {
-	resp, err := utils.MakeMetoroAPIRequest("GET", "k8s/events/summaryAttributes", nil, utils.GetAPIRequirementsFromRequest(ctx))
+	path, err := buildGetK8sEventsSummaryAttributesPath(arguments)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := utils.MakeMetoroAPIRequest("GET", path, nil, utils.GetAPIRequirementsFromRequest(ctx))
 	if err != nil {
 		return nil, err
 	}
 
 	return mcpgolang.NewToolResponse(mcpgolang.NewTextContent(fmt.Sprintf("%s", string(resp)))), nil
+}
+
+func buildGetK8sEventsSummaryAttributesPath(arguments GetK8sEventsAttributesHandlerArgs) (string, error) {
+	startTime, endTime, err := utils.CalculateTimeRange(arguments.TimeConfig)
+	if err != nil {
+		return "", fmt.Errorf("error calculating time range: %v", err)
+	}
+
+	query := url.Values{}
+	query.Set("startTime", strconv.FormatInt(startTime, 10))
+	query.Set("endTime", strconv.FormatInt(endTime, 10))
+
+	return "k8s/events/summaryAttributes?" + query.Encode(), nil
 }
